15bb9ed298611b694b639f4db80c4593cbd36282
Sean Puller <43151199+SeanPuller@users.noreply.github.com>
2024-06-17 20:04:49 +0200

    add install file for faster windows development

---
+++ install.bat
@@ -0,0 +1,17 @@
+@echo off
+::Set Folders
+set SOURCE_FOLDER=docker_example
+set SOURCE_FILE=docker_example.desktop
+set DESTINATION=%APPDATA%\krita\pykrita
+set DESTINATIONFOLDER=%APPDATA%\krita\pykrita\docker_Example
+
+::Remove folder and files if they exist already
+if exist del /q "%APPDATA%\Krita\Pykrita\docker_example.desktop"
+if exist rd /s /q "%APPDATA%\Krita\Pykrita\docker_example"
+
+::Create docker_example folder
+if not exist "%APPDATA%\Krita\Pykrita\docker_example" mkdir "%APPDATA%\Krita\Pykrita\docker_Example"
+
+::Add files
+xcopy /s /y %SOURCE_FOLDER% %DESTINATIONFOLDER%
+copy /y "%SOURCE_FILE%" %DESTINATION%
