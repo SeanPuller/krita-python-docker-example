482a47c65bb35d487607444895b3d221ac06c888
Sean Puller <43151199+SeanPuller@users.noreply.github.com>
2024-06-17 20:29:12 +0200

    add button to widget

--- docker_example/docker_example.py
+++ docker_example/docker_example.py
@@ -6,6 +6,17 @@ class docker_example(DockWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Docker Example")
-
+        mainWidget = QWidget(self)
+        self.setWidget(mainWidget)
+        
+        exampleButton = QPushButton("Example Button",mainWidget)
+        exampleButton.clicked.connect(self.popup)
+        
+        mainWidget.setLayout(QVBoxLayout())
+        mainWidget.layout().addWidget(exampleButton)
+        
+    def popup(self):
+              QMessageBox.information(QWidget(), "Docker Example", "This example button works")
+              
     def canvasChanged(self, canvas):
         pass
